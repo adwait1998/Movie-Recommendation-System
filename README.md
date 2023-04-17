5a359852fb237e93a20724121c4b3115322dd933
adwait1998 <43320637+adwait1998@users.noreply.github.com>
2023-04-17 01:19:33 -0700

    Movie Recommendation System Code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Movie Recommendation System
- SWM Final Project
+# CSE573- SWM Project
+
+## Movie-Recommendation
+
+Group 1: Fall 2022 Semester
+
+## Members:
+
+1. Aditi Joshi
+2. Kaushik Hegde
+3. Harshita Verma
+4. Mamatha Juluru
+5. Kavya Bathini
+6. Vishrut Jha
---
+++ UI/imagescraper.py
@@ -0,0 +1,51 @@
+# Get Movie Poster from IMDB from the Movie Title
+import json
+from bs4 import BeautifulSoup
+import requests
+import os
+from PIL import Image
+from io import BytesIO
+
+
+class ImageScraper:
+    def beautiful_soup(self, url):
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+    }
+        response = requests.get(url,headers=headers)
+        return BeautifulSoup(response.content, "html.parser")
+
+    def get_IMDb_ID(self, title):
+        url = f"http://www.imdb.com/find?s=tt&q={title}"
+        soup = self.beautiful_soup(url)
+        if result := soup.find("a", {"class": "ipc-metadata-list-summary-item__t"}):
+            return result.get("href").split("/")[2]
+        print(re)
+        return None
+
+    def get_movie_info_IMDb(self, title):
+        imdb_id = self.get_IMDb_ID(title)
+        if not imdb_id:
+            return None
+        url = f"https://www.imdb.com/title/{imdb_id}/?ref_=fn_tt_tt_1"
+        soup = self.beautiful_soup(url)
+        return json.loads(
+            str(soup.findAll("script", {"type": "application/ld+json"})[0].text)
+        )
+
+    def get_poster_url(self, title):
+        if movie_info := self.get_movie_info_IMDb(title):
+            print(movie_info)
+            return movie_info["image"]
+        return None
+
+    def download_poster(self, title, dir_name):
+        if src := self.get_poster_url(title):
+            if not os.path.isdir(dir_name):
+                os.mkdir(dir_name)
+            response = requests.get(src)
+            ext = src.split(".")[-1]
+            img = Image.open(BytesIO(response.content))
+            img.save(f"{dir_name}/{title}.{ext}")
+        else:
+            print(f"No poster found for {title}")
